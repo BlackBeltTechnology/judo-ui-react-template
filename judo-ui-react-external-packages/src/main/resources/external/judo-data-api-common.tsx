5e6e9dead78875daa3f49790fc9371b08a733e07
Norbert Csaba Herczeg <noherczeg@gmail.com>
2023-04-11 10:28:08 +0200

    JNG-4730 simplify react architecture (#51)
    * JNG-4730 simplify react architecture
    
    * JNG-4730 update versions
    
    * JNG-4730 tidy docs
    
    * JNG-4730: fix conflicts
    
    * JNG-4730: extend readme
    
    * JNG-4730: extend readme

--- judo-ui-react-external-packages/src/main/resources/external/judo-data-api-common.tsx
+++
@@ -1,1 +0,0 @@
-export * from '@judo/data-api-common';
---
+++ judo-ui-react/src/main/resources/actor/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          '@mui/material': ['@mui/material'],
+          '@mui/x-data-grid': ['@mui/x-data-grid'],
+          '@mui/x-date-pickers': ['@mui/x-date-pickers'],
+          'axios': ['axios'],
+          'oidc-client-ts': ['oidc-client-ts'],
+          'uuid': ['uuid'],
+        },
+      },
+    },
+  },
+  plugins: [
+      react(),
+  ],
+});
