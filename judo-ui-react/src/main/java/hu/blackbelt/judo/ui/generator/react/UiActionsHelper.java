2ea8eb6c9b962cde1740b7ca894216b41218211b
Norbert Csaba Herczeg <noherczeg@gmail.com>
2023-12-20 18:31:27 +0100

    JNG-5363 fix association relation refresh actions (#267)

--- judo-ui-react/src/main/java/hu/blackbelt/judo/ui/generator/react/UiActionsHelper.java
+++ judo-ui-react/src/main/java/hu/blackbelt/judo/ui/generator/react/UiActionsHelper.java
@@ -94,9 +94,6 @@ public class UiActionsHelper {
     }
 
     public static ActionDefinition getRefreshActionDefinitionForTable(Table table) {
-        if (table.getRelationType() != null && !table.getRelationType().getIsRelationKindAssociation()) {
-            return null;
-        }
         return (ActionDefinition) table.getTableActionDefinitions().stream().filter(a -> ((ActionDefinition) a).getIsRefreshAction()).findFirst().orElse(null);
     }
 
