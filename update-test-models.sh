4041961859c197f98f69b47d4d60f91859c3b8e8
Norbert Herczeg <noherczeg@gmail.com>
2024-05-03 20:44:01 +0200

    JNG-5711 update models, remove unused ones

--- update-test-models.sh
+++ update-test-models.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-cp ../judo-tatami-tests/models/ActionGroupTest/application/frontend-react/model/ActionGroupTest-ui.model ./judo-ui-react-itest/ActionGroupTest/model/ActionGroupTest-ui.model
-cp ../judo-tatami-tests/models/ActionGroupTest/application/frontend-react/model/ActionGroupTest-ui.model ./judo-ui-react-itest/ActionGroupTestPro/model/ActionGroupTestPro-ui.model
-cp ../judo-tatami-tests/models/BinaryTypeTest/application/frontend-react/model/BinaryTypeTest-ui.model ./judo-ui-react-itest/BinaryTypeTest/model/BinaryTypeTest-ui.model
-cp ../judo-tatami-tests/models/ComponentOverrideTest/application/frontend-react/model/ComponentOverrideTest-ui.model ./judo-ui-react-itest/ComponentOverrideTest/model/ComponentOverrideTest-ui.model
-cp ../judo-tatami-tests/models/CreatePagesTest/application/frontend-react/model/CreatePagesTest-ui.model ./judo-ui-react-itest/CreatePagesTest/model/CreatePagesTest-ui.model
-cp ../judo-tatami-tests/models/CRUDActionsTest/application/frontend-react/model/CRUDActionsTest-ui.model ./judo-ui-react-itest/CRUDActionsTest/model/CRUDActionsTest-ui.model
-cp ../judo-tatami-tests/models/OperationFaultTest/application/frontend-react/model/OperationFaultTest-ui.model ./judo-ui-react-itest/OperationFaultTest/model/OperationFaultTest-ui.model
-cp ../judo-tatami-tests/models/OperationParametersTest/application/frontend-react/model/OperationParametersTest-ui.model ./judo-ui-react-itest/OperationParametersTest/model/OperationParametersTest-ui.model
-cp ../judo-tatami-tests/models/ReadOnlyTest/application/frontend-react/model/ReadOnlyTest-ui.model ./judo-ui-react-itest/ReadOnlyTest/model/ReadOnlyTest-ui.model
+cp ../../runtime/judo-tatami-tests/models/ActionGroupTest/application/frontend-react/model/ActionGroupTest-ui.model ./judo-ui-react-itest/ActionGroupTest/model/ActionGroupTest-ui.model
+cp ../../runtime/judo-tatami-tests/models/ActionGroupTest/application/frontend-react/model/ActionGroupTest-ui.model ./judo-ui-react-itest/ActionGroupTestPro/model/ActionGroupTestPro-ui.model
+cp ../../runtime/judo-tatami-tests/models/CRUDActionsTest/application/frontend-react/model/CRUDActionsTest-ui.model ./judo-ui-react-itest/CRUDActionsTest/model/CRUDActionsTest-ui.model
+cp ../../runtime/judo-tatami-tests/models/OperationParametersTest/application/frontend-react/model/OperationParametersTest-ui.model ./judo-ui-react-itest/OperationParametersTest/model/OperationParametersTest-ui.model
