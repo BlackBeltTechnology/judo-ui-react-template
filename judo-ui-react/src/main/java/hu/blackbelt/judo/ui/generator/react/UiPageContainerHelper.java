7e226a4536fc76f6f11d16177d3c34056be8a5eb
Norbert Csaba Herczeg <noherczeg@gmail.com>
2024-06-12 20:34:49 +0200

    JNG-5768 operation selector submit fix

--- judo-ui-react/src/main/java/hu/blackbelt/judo/ui/generator/react/UiPageContainerHelper.java
+++ judo-ui-react/src/main/java/hu/blackbelt/judo/ui/generator/react/UiPageContainerHelper.java
@@ -414,6 +414,10 @@ public class UiPageContainerHelper {
         }
         segments.add("isLoading");
 
+        if (container.isIsSelector() && button.getActionDefinition().getIsCallOperationAction()) {
+            segments.add("!selectionDiff.length");
+        }
+
         return String.join(" || ", segments);
     }
 
