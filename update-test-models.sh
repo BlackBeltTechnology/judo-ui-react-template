6e8efd1f1841bea35849e08b3ddbb510446dc269
Norbert Csaba Herczeg <noherczeg@gmail.com>
2025-11-13 22:29:50 +0100

    add view context provider

--- update-test-models.sh
+++ update-test-models.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-cp ../../runtime/judo-tatami-tests/models/ActionGroupTest/application/frontend-react/model/ActionGroupTest-ui.model ./judo-ui-react-itest/ActionGroupTest/model/ActionGroupTest-ui.model
-cp ../../runtime/judo-tatami-tests/models/ActionGroupTest/application/frontend-react/model/ActionGroupTest-ui.model ./judo-ui-react-itest/ActionGroupTestPro/model/ActionGroupTestPro-ui.model
-cp ../../runtime/judo-tatami-tests/models/CRUDActionsTest/application/frontend-react/model/CRUDActionsTest-ui.model ./judo-ui-react-itest/CRUDActionsTest/model/CRUDActionsTest-ui.model
-cp ../../runtime/judo-tatami-tests/models/OperationParametersTest/application/frontend-react/model/OperationParametersTest-ui.model ./judo-ui-react-itest/OperationParametersTest/model/OperationParametersTest-ui.model
-cp ../../runtime/judo-tatami-tests/models/RelationTest/application/frontend-react/model/RelationTest-ui.model ./judo-ui-react-itest/RelationTest/model/RelationTest-ui.model
-cp ../../runtime/judo-tatami-tests/models/SimpleOrderManagement/application/frontend-react/model/SimpleOrderManagement-ui.model ./judo-ui-react-itest/SimpleOrderManagement/model/SimpleOrderManagement-ui.model
+cp ../judo-ng/runtime/judo-tatami-tests/models/ActionGroupTest/application/frontend-react/model/ActionGroupTest-ui.model ./judo-ui-react-itest/ActionGroupTest/model/ActionGroupTest-ui.model
+cp ../judo-ng/runtime/judo-tatami-tests/models/ActionGroupTest/application/frontend-react/model/ActionGroupTest-ui.model ./judo-ui-react-itest/ActionGroupTestPro/model/ActionGroupTestPro-ui.model
+cp ../judo-ng/runtime/judo-tatami-tests/models/CRUDActionsTest/application/frontend-react/model/CRUDActionsTest-ui.model ./judo-ui-react-itest/CRUDActionsTest/model/CRUDActionsTest-ui.model
+cp ../judo-ng/runtime/judo-tatami-tests/models/OperationParametersTest/application/frontend-react/model/OperationParametersTest-ui.model ./judo-ui-react-itest/OperationParametersTest/model/OperationParametersTest-ui.model
+cp ../judo-ng/runtime/judo-tatami-tests/models/RelationTest/application/frontend-react/model/RelationTest-ui.model ./judo-ui-react-itest/RelationTest/model/RelationTest-ui.model
+cp ../judo-ng/runtime/judo-tatami-tests/models/SimpleOrderManagement/application/frontend-react/model/SimpleOrderManagement-ui.model ./judo-ui-react-itest/SimpleOrderManagement/model/SimpleOrderManagement-ui.model
